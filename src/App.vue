c40b02f65efecf5dda7396bfd1d12a78102c4a5e
André Cabral <andrecabrall@gmail.com>
2021-05-16 15:29:26 -0300

    Primeiro Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <caixa-eletronico></caixa-eletronico>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CaixaEletronico from './components/CaixaEletronico.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CaixaEletronico
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueResource from 'vue-resource'
+import VueRouter from 'vue-router'
+import routes from './routes'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+Vue.use(VueResource)
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+    mode: 'history',
+    routes
+
+
+})
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
+    router
+
 }).$mount('#app')
